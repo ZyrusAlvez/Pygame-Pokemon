67b99a708d46456c483ba0ad843a84977316f813
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2024-11-21 14:28:19 +0800

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pokemon import *
 
 # Pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1100, 600))
+screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
 
@@ -15,39 +15,58 @@ scale_factor3 = 1
 
 # Array to store pokemons
 pokemons = [butterfree, charizard, dugtrio, golbat, kadabra, meowth, nidoking, pidgeot, pikachu, venonat, venusaur, wartortle]
+player1_pokemons = []
+player2_pokemons = []
 
 # store the images in a dictionary
 loaded_images = {}
-for i, pokemon in enumerate(pokemons, 1):
+for i, pokemon in enumerate(pokemons):
     loaded_images[i] = [pygame.image.load(frame) for frame in pokemon.animation_frames()]
 
 # makes sure that the index wont go beyond to the count of frames   
 pokemon_frame_index = {}
-for i, pokemon in enumerate(pokemons, 1):
+for i, pokemon in enumerate(pokemons):
     pokemon_frame_index[i] = 0
     
+update = False
+focus = 2
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                player1_pokemons.append(pokemons[1])
+                
+                         
     # background
     screen.fill((255, 255, 255))
     
-    # Get the image's rectangle (bounding box) and place it on the screen using its center
-    pokemon1_image_rect = loaded_images[1][pokemon_frame_index[1]].get_rect(center=((screen.get_width() // 2, screen.get_height() // 2)))
-    pokemon2_image_rect = loaded_images[2][pokemon_frame_index[2]].get_rect(center=((screen.get_width() // 2 + 200, screen.get_height() // 2)))
-    pokemon3_image_rect = loaded_images[3][pokemon_frame_index[3]].get_rect(center=((screen.get_width() // 2 - 200, screen.get_height() // 2)))
-    pokemon4_image_rect = loaded_images[4][pokemon_frame_index[4]].get_rect(center=((screen.get_width() // 2 + 400, screen.get_height() // 2)))
-    pokemon5_image_rect = loaded_images[5][pokemon_frame_index[5]].get_rect(center=((screen.get_width() // 2 - 400, screen.get_height() // 2)))
-    
-    # showed pokemon
-    screen.blit(loaded_images[1][pokemon_frame_index[1]], pokemon1_image_rect)
-    screen.blit(loaded_images[2][pokemon_frame_index[2]], pokemon2_image_rect)
-    screen.blit(loaded_images[3][pokemon_frame_index[3]], pokemon3_image_rect)
-    screen.blit(loaded_images[4][pokemon_frame_index[4]], pokemon4_image_rect)
-    screen.blit(loaded_images[5][pokemon_frame_index[5]], pokemon5_image_rect)
+    if not focus:
+        pass
+    elif focus == 1:
+        pokemon1_image_rect = loaded_images[0][pokemon_frame_index[0]].get_rect(center=((screen.get_width() // 2 - 200, screen.get_height() // 2)))
+        pokemon2_image_rect = loaded_images[1][pokemon_frame_index[1]].get_rect(center=((screen.get_width() // 2, screen.get_height() // 2)))
+        pokemon3_image_rect = loaded_images[2][pokemon_frame_index[2]].get_rect(center=((screen.get_width() // 2 + 200, screen.get_height() // 2)))
+        pokemon4_image_rect = loaded_images[3][pokemon_frame_index[3]].get_rect(center=((screen.get_width() // 2 + 400, screen.get_height() // 2)))
+        screen.blit(loaded_images[0][pokemon_frame_index[0]], pokemon1_image_rect)
+        screen.blit(loaded_images[1][pokemon_frame_index[1]], pokemon2_image_rect)
+        screen.blit(loaded_images[2][pokemon_frame_index[2]], pokemon3_image_rect)
+        screen.blit(loaded_images[3][pokemon_frame_index[3]], pokemon4_image_rect)
+    else:
+        pokemon1_image_rect = loaded_images[0][pokemon_frame_index[0]].get_rect(center=((screen.get_width() // 2 - 400, screen.get_height() // 2)))
+        pokemon2_image_rect = loaded_images[1][pokemon_frame_index[1]].get_rect(center=((screen.get_width() // 2 - 200, screen.get_height() // 2)))
+        pokemon3_image_rect = loaded_images[2][pokemon_frame_index[2]].get_rect(center=((screen.get_width() // 2, screen.get_height() // 2)))
+        pokemon4_image_rect = loaded_images[3][pokemon_frame_index[3]].get_rect(center=((screen.get_width() // 2 + 200, screen.get_height() // 2)))
+        pokemon5_image_rect = loaded_images[4][pokemon_frame_index[4]].get_rect(center=((screen.get_width() // 2 + 400, screen.get_height() // 2)))
+        screen.blit(loaded_images[0][pokemon_frame_index[0]], pokemon1_image_rect)
+        screen.blit(loaded_images[1][pokemon_frame_index[1]], pokemon2_image_rect)
+        screen.blit(loaded_images[2][pokemon_frame_index[2]], pokemon3_image_rect)
+        screen.blit(loaded_images[3][pokemon_frame_index[3]], pokemon4_image_rect)
+        screen.blit(loaded_images[4][pokemon_frame_index[4]], pokemon5_image_rect)
+
     
     # update the screen
     pygame.display.flip()
